d8f1864017587227c033817683a7be9988c1c67d
placementwallahofficial <placementwallahofficial@gmail.com>
2022-08-24 18:10:49 +0530

    V 0.0.1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../logo.svg';
+// import logo from '../../logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
@@ -10,19 +10,19 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          {/* <img src={logo} /> */}
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#wgpt3">Why BygBrain?</a></p>
+          <p><a href="#possibility">Expert Solutions</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
+        {/* <p>Sign in</p>
+        <button type="button">Sign up</button> */}
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu
@@ -38,8 +38,8 @@ const Navbar = () => {
             <p><a href="#blog">Library</a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
+            {/* <p>Sign in</p>
+            <button type="button">Sign up</button> */}
           </div>
         </div>
         )}
